954c6e4f7b438928e9f56128215d389edc4b8a17
maaaaaaaaad <integral5137@gmail.com>
2026-01-03 00:12:29 +0900

    .

---
+++ Assets/Scripts/Managers/Manager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class Manager : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Manager script started");
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
